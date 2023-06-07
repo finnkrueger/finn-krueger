886a402ce28c1bb77f10bbd81a6ea82ea0830239
finnkrueger <fkrueger@inbox.lv>
2023-06-07 18:26:54 +0200

    ddd

--- Untitled.R
+++ Untitled.R
@@ -111,10 +111,14 @@ checking2_foreign <-data_master%>%
   print()
 
 
-### take percent
+### logging financial data
 
-data_master1$Financial_Open <- data_master1$Financial_Open*100
-data_master1$IIP_GDP <- data_master1$IIP_GDP*100
+
+data_master$Financial_Open_Logged <- log(data_master$Financial_Open)
+data_master$Foreign_share_logged <- log(data_master$Foreign_share)
+
+
+
 
 ##### GRAPHS
 
@@ -142,21 +146,250 @@ var.labs <- data.frame(var = c('p05','p50',
                                   'Preferences of the Median',
                                   'Preferences of the top 5%',
                                   'Averange Change in Welfare Generosity',
-                                  'Welfare Generosity',
+                                  'TotalWelfare Generosity',
                                   'Captial Mobility Logged',
                                   'Net international investment Position (% of GDP)',
                                   'Logged GDP',
                                   'GDP growth',
                                   'Unemployment'
                                 ))
-  
-?st
+
 
 st(summary_stats, labels = var.labs, title = 'Table 1: Summary Statistics')
-st(summary_stats, labels = var.labs, title = 'Table 1: Summary Statistics', note = 'Table 1: Summary Statistics' out = 'csv',file = "/Users/finnkruger/Documents/Hertie/Masterarbeit/Respog/other/Graphs & Tables")
+
+
+### Taking care of Outliers 
+
+data_master1 <- data_master1%>%
+  filter(Financial_Open <= 10)
+
+
+data_master1$Financial_Open
+
+unique(data_master1$country2)
+
+data_master1 <- data_master1%>%
+  filter(country2 != 'Ireland' & country2 != 'Netherlands')
+
+
+data_master1 <- data_master%>%
+  filter(!is.na(dgentav14))%>%
+  filter(dgentav14 >= -20)
+g
+
+data_master1 <- data_master1%>%
+  filter(IIP == 'positive')
+
+### Replicate Real but unequal responsiveness
+
+
+original_model <- lmer(dgentav14 ~ p05 + p95+ gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master, REML = FALSE)
+
+original_model1 <- lmer(dgentav14 ~ p95+ gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+original_model2 <- lmer(dgentav14 ~ p50 + gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+original_model3 <- lmer(dgentav14 ~ p05 + gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+summary(original_model)
+
+model_parameters(original_model)
+
+### Robust standard errors 
+
+check_heteroskedasticity(original_model)
+
+cstypes <- paste0("CR", c("0", "1", "1p", "1S", "2", "3"))
+rob_se_fun <- function(type) sqrt(diag(vcovCR(original_model, type = type)))
+
+rob_se <- sapply(cstypes, rob_se_fun)
+std_se <- sqrt(diag(vcov(original_model)))
+cbind(std = std_se, rob_se_fun,
+      merDeriv = sqrt(diag(sand)[1:2]))
+
+coef_test(original_model, vcov = "CR1", p_values = TRUE, test = "naive-t")
+
+
+### interacting with financial openness
+
+
+model <- lmer(dgentav14 ~ p95*Financial_Open_Logged + IIP_GDP + gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+model_within <- lm(dgentav14 ~ p95*Financial_Open_Logged + IIP_GDP + gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + as.factor(country), data = data_master1)
+
+model2 <- lmer(dgentav14 ~ p05*Financial_Open_Logged + IIP_GDP + gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+model3 <- lmer(dgentav14 ~ p50*Financial_Open_Logged + IIP_GDP + gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+model4 <- lmer(dgentav14 ~ Rich_vs_Poor*Financial_Open_Logged + IIP_GDP +  gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+
+###experimenting with other models 
+
+model5 <- lmer(dgentav14 ~ p05*Financial_Open_Logged + Financial_Open_Logged*p95 + IIP_GDP + gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+model7 <- lmer(dgentav14 ~ p05 + p95 + Financial_Open_Logged +  gent + loggdpt + growtht + unempt + factor(topic) + factor(wave) + (1 | country), data = data_master1, REML = FALSE)
+
+
+#### Summary of models 
+
+## Robust Standard Erorrs
+RSE_Model <- vcovCR(model, type = "CR1")
+RSE_Model2 <- vcovCR(model2, type = "CR1")
+RSE_Model3 <- vcovCR(model3, type = "CR1")
+
+RSE_Model5 <- vcovCR(model5, type = "CR1")
+
+### Checking models 
+
+coef_test(model_within, vcov = "CR1", p_values = TRUE)
+coef_test(model2, vcov = "CR1", p_values = TRUE)
+
+model_parameters(model_within, vcov = RSE_Model)
+
+
+model_parameters(model, vcov = RSE_Model)
+model_parameters(model2, vcov = RSE_Model2)
+model_parameters(model5, vcov = RSE_Model5)
+
+
+vcov = list(RSE_Model, RSE_Model2, RSE_Model3)
+models1 = list(model, model2, model3)
+
+
+countries <- list(unique(data_master1$country))
+
+#### Creating Output table
+
+cm <- c('(Intercept)' = 'Intercept',
+        'p95' = 'Preferences of the richest 5%',
+        'p05'    = 'Preferences of the poorest 5%',
+        'p50' = 'Preferences of the median',
+        'Financial_Open_Logged' = 'Logged Financial Openness',
+        'IIP_GDP'  = 'International Investment Position',
+        'p95:Financial_Open_Logged' = 'Preferences P95',
+        'p50:Financial_Open_Logged' = 'Preferences P50 x Logged Financial Openness',
+        'p05:Financial_Open_Logged' = 'Preferences P05 x Logged Financial Openness')
+
+summary(model)
+
+modelsummary(model, vcov = RSE_Model, stars = TRUE)
+modelsummary(models1, vcov = vcov, stars = TRUE, coef_map = cm)
+modelsummary(models1, vcov = vcov, stars = TRUE)goo
+
+
+?modelsummary
+
+### plot 
+
+plot_predictions(model, rug = TRUE, condition = c("p95", "Financial_Open_Logged"), vcov = RSE_Model)
+plot_predictions(model, rug = TRUE, condition = c("p95", "Financial_Open_Logged"))
+
+plot_predictions(model2, rug = TRUE, condition = c("p05", "Financial_Open_Logged"), , vcov = RSE_Model2)
+
+plot_predictions(model3, rug = TRUE, condition = c("p50", "Financial_Open_Logged"))
+
+
+plot_model(model4, type = "int", terms = c("Rich_vs_Poor", "Financial_Open_Logged"))
+
+
+plot_predictions(model6, rug = TRUE, condition = c("p95", "Financial_Open_Logged"))
+
+### Plotting Marginal Means
+
+
+#plot_model(model, type = "pred", terms = c("p95", "Financial_Open_Logged"))
+
+plot_model(model, type = "int", terms = c("p95", "Financial_Open_Logged"), show.data = TRUE, Rug) + 
+  geom_point(data = data_master1, aes(x = p95, y = dgentav14, colour = Financial_Open_Logged), inherit.aes = FALSE) +
+  scale_color_continuous()
+
+plot_model(model2, type = "int", terms = c("p05", "Financial_Open_Logged"), show.data = TRUE, vcov.fun = RSE_Model2) + 
+  geom_point(data = data_master1, aes(x = p05, y = dgentav14, colour = Financial_Open_Logged), inherit.aes = FALSE) +
+  scale_fill_gradient2("Financial_Open_Logged", limits = c(2, 2.5), 
+                       low = "#762A83", mid = "white", high = "#1B7837") 
+
+
+###partial regression plot -> added variable plot. 
+
+
+plot_model(model, type = "int", terms = c("p95", "Financial_Open_Logged"), show.data = TRUE)+ geom_rug(alpha = 1/2, position = "jitter")
+plot_model(model, vcov.fun = RSE_Model, type = "int", terms = c("p95", "Financial_Open_Logged"), show.data = TRUE)+ geom_rug(alpha = 1/2, position = "jitter")
+
+
+plot_model(model2, vcov.fun = RSE_Model2, type = "int", terms = c("p05", "Financial_Open_Logged"))+ geom_rug(alpha = 1/2, position = "jitter")
+plot_model(model2, type = "int", terms = c("p05", "Financial_Open_Logged"))+ geom_rug(alpha = 1/2, position = "jitter")
+
+plot_model(model3, type = "int", terms = c("Financial_Open_Logged", "p50"))+ geom_rug()
+
+
+plot_model(model6, type = "int", terms = c("p50", "Financial_Open_Logged"))+ geom_rug()
+
+
+
+#plot_model(model1, type = "pred", terms = c("Financial_Open_Logged", "p05"))
+
+with(data_master1, scatter.smooth(year, dgentav14))
+
+
+hist()
+
+plot_model(model, type = "int", terms = c("p95", "Financial_Open_Logged")) + 
+  geom_rug(data = subset(data_master1, Financial_Open_Logged > 1), aes(x = Financial_Open_Logged,y = dgentav14), color = "black", sides = "b", , inherit.aes = FALSE ) 
+
+
+
+### checking for diagnosis 
+
+
+plot_model(model, type = "re", terms = c("Financial_Open_Logged"))
+
+plot_model(model, type = "re")
+
+#
+plot_model(model2, type = 'slope', terms = c('Financial_Open_Logged')) + geom_rug()
+
+
+plot_model(original_model, type = "diag", sort.est = '(Intercept)')
+
+### checking for endogenetiy 
+endogeneity_model <- lm(p95 ~ Financial_Open_Logged, data = data_master1)
+endogeneity_model2 <- lm(dgentav14 ~ Financial_Open_Logged, data = data_master1)
+
+
+
+summary(endogeneity_model)
+
+## PLOTTING SLOPES AND COMPARISONS MARGINAL EFFECTS 
+
+
+plot_slopes(model, variables = "p95", vcov = RSE_Model, condition = c("Financial_Open_Logged"))
+
+plot_slopes(model, variables = "p95", condition = c("Financial_Open_Logged"))
+
+plot_slopes(model2, variables = "p05", vcov = RSE_Model2, condition = c("Financial_Open_Logged"))
+plot_slopes(model2, variables = "p05", condition = c("Financial_Open_Logged"))
+
+plot_slopes(model3, variables = "p50", condition = c("Financial_Open_Logged"))
+
+plot_slopes(model6, variables = "p95", condition = c("Financial_Open_Logged"))
+
+hist(data_master1$Financial_Open)
+hist(data_master1$Financial_Open_Logged)
+hist(data_master1$IIP_GDP)
+
+avg_slopes(model2, variables = "p05", condition = c("Financial_Open_Logged"))
+
+plot_comparisons(model2, variables = "p05", vcov = RSE_Model2, condition = c("Financial_Open_Logged"))
+plot_comparisons(model2, variables = "p05", condition = c("Financial_Open_Logged"))
+
+plot_comparisons(model, variables = "p95",  condition = c("Financial_Open_Logged"))
+
+
 
 ####
 
 
 res <- cor(data_master1$dg)
 round(res, 2)
+
